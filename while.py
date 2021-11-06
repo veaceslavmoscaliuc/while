f95b1d303044c1951d1fcbcba1fdee8b827b4ee2
veaceslavmoscaliuc <93546875+veaceslavmoscaliuc@users.noreply.github.com>
2021-11-06 16:13:46 +0200

    Add files via upload

---
+++ while.py
@@ -0,0 +1,30 @@
+
+
+# * * # * * # * * #.....
+symbol =1
+
+print ("\n")
+
+while symbol <= 10:
+    if symbol % 3 == 1:
+        print("#", end=" ")
+    else:
+        print("*", end=" " )
+    symbol += 1
+
+
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
